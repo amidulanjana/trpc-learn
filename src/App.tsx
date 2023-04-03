ea80f23a8314ef412a5b1c4c78d8d9a774d86a17
amiladulanjana <amiladulanjana@hotmail.com>
2023-04-03 15:36:01 +0530

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
+import { trpc } from './trpc';
 
 function App() {
+
+  const [companies,setCompanies] = useState<{ name: string;id: string;}[]>([])
+  const companyNameRef = useRef<HTMLInputElement | null>(null)
+
+  const getCompanies = async () => {
+    var data = await trpc.company.query()
+    const companyList = data.map(d => ({...d}))
+    setCompanies(companyList)
+  }
+
+  const addCompany = () => {
+    if(companyNameRef.current){
+      trpc.createCompany.mutate({id: new Date().getTime().toString(), name:companyNameRef.current.value})
+      getCompanies()
+    }
+  }
+
+  useEffect(() => {
+    getCompanies()
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <input type='text' placeholder='company' ref={companyNameRef}/>
+        <button type='button' onClick={addCompany}>Add company</button>
+      </section>
+      {companies.map(c => <h3 key={c.id}>{c.name}</h3>)}
     </div>
   );
 }
